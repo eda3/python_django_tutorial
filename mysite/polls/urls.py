6643920735a7a9cf9e4bb5ee767c8aa8aa343d30
eda3 <gerorin1010@gmail.com>
2021-03-20 09:16:49 +0900

    最初のページと管理画面表示

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,7 @@
-from django.urls import pathspec
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path("", view.index, name="index"),
+    path("", views.index, name="index"),
 ]
